8273acdaf3dd4b732aae34aada3aa258898c7eb8
13rian Vargas <brianstevenvargasclavijo@gmail.com>
2024-04-22 10:31:13 -0500

    Se añade el código para la clase Principal.

--- src/main/java/com/mycompany/libropoo/Principal.java
+++ src/main/java/com/mycompany/libropoo/Principal.java
@@ -1,5 +1,38 @@
 package com.mycompany.libropoo;
 
+import java.util.Scanner;
+
 public class Principal {
+    public static void main(String[] args) {//Complejidad Temporal: Tiempo constante O(1)
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Ingrese el título:");
+        String title = sc.nextLine();
+        System.out.println("Ingrese el autor:");
+        String autor = sc.nextLine();
+        System.out.println("Ingrese el género:");
+        String gender = sc.nextLine();
+        System.out.println("Ingrese el año de publicación:");
+        int anio = sc.nextInt();
+
+        Libro libro = new Libro(title, autor, gender, anio);
+
+        libro.marcarLeido();
+
+        System.out.println(libro.mostrarInformacion());
+
+        if (libro.leido == true){
+            System.out.println("Ya fue leido.");
+        }else{
+            System.out.println("No fue leido.");
+        }//cierra if
     
-}
+        if (libro.esAntiguo() == true){
+            System.out.println("El libro es antiguo.");
+        }else{
+            System.out.println("El libro no es antiguo.");
+        }//cierra if
+
+        sc.close();
+    }//cierra el main
+}//cierra la clase principal
